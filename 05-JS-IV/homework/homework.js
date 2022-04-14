b621fa37577b0be956f781856c93e0077ced26dc
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-04-13 23:50:29 -0300

    js4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -157,7 +157,7 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-producto.
+producto
 }
 
 // No modificar nada debajo de esta línea
