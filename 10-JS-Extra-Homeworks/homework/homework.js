64e92c1678e53168d112da2e49c074e967af5716
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2022-05-26 15:28:36 -0300

    hi

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  let array = [];
+  for (var clave in objeto){
+      array.push([clave, objeto[clave]])
+  }
+  return array;
 }
 
 
@@ -18,7 +23,15 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
-}
+  var objeto = {}
+  for( let i=0; i<string.length; i++){
+    if (objeto.hasOwnProperty(string[i])){ 
+        objeto[string[i]]=objeto[string[i]] + 1
+      } else {
+        objeto[string[i]] = 1
+      } 
+    }   return objeto
+  }
 
 
 function capToFront(s) {
